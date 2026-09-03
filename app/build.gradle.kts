f20c7cdbd397caec3630d4ce7c3f70608056316c
Timo <none>
2026-09-03 13:58:37 +0300

    Release 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "fi.tukkateatteri"
         minSdk = 30
         targetSdk = 37
-        versionCode = 3
-        versionName = "0.1.1"
+        versionCode = 4
+        versionName = "0.2.0"
 
     }
 
